d1985a025f57afa4b916c35684d56f0be433101b
notmakey <34926863+notmakey@users.noreply.github.com>
2018-02-27 12:03:02 +1100

    Update fff.js

--- fff.js
+++ fff.js
@@ -1,17 +1,54 @@
-function save(){
-    var checkbox = document.getElementById('checkbox1zaal1');
-    localStorage.setItem('checkbox1zaal1', checkbox.checked);
-}
+//===== Cookies Plugin=====   //
 
-function load(){    
-    var checked = JSON.parse(localStorage.getItem('checkbox1zaal1'));
-    document.getElementById("checkbox1zaal1").checked = checked;
-}
+(function ($) {
+            $.cookie = function (key, value, options) {
 
-function wis(){
-    location.reload();
-    localStorage.clear()
+                // key and at least value given, set cookie...
+                if (arguments.length > 1 && (!/Object/.test(Object.prototype.toString.call(value)) || value === null || value === undefined)) {
+                    options = $.extend({}, options);
 
-}
+                    if (value === null || value === undefined) {
+                        options.expires = -1;
+                    }
 
-load();
+                    if (typeof options.expires === 'number') {
+                        var days = options.expires, t = options.expires = new Date();
+                        t.setDate(t.getDate() + days);
+                    }
+
+                    value = String(value);
+
+                    return (document.cookie = [
+                encodeURIComponent(key), '=', options.raw ? value : encodeURIComponent(value),
+                options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+                options.path ? '; path=' + options.path : '',
+                options.domain ? '; domain=' + options.domain : '',
+                options.secure ? '; secure' : ''
+            ].join(''));
+                }
+
+                // key and possibly options given, get cookie...
+                options = value || {};
+                var decode = options.raw ? function (s) { return s; } : decodeURIComponent;
+
+                var pairs = document.cookie.split('; ');
+                for (var i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
+                    if (decode(pair[0]) === key) return decode(pair[1] || ''); // IE saves cookies with empty string as "c; ", e.g. without "=" as opposed to EOMB, thus pair[1] may be undefined
+                }
+                return null;
+            };
+        })(jQuery);
+
+//======================================//
+
+  $(document).ready(function () {
+            var checkbox = $('#boxlawreg').find(':checkbox'), checkboxCookieName = 'checkbox-state';
+
+            checkbox.each(function () {
+                $(this).attr('checked', $.cookie(checkboxCookieName + '|' + $(this).attr('name')));
+            });
+
+            checkbox.click(function () {                
+                $.cookie(checkboxCookieName + '|' + $(this).attr('name'), $(this).prop('checked'));
+            });
+        });
